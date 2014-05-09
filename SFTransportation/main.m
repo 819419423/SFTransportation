296e9f9b1fa072aad248c089a3a074b0e42ae52a
819419423 <daijing24@gmail.com>
2014-05-08 18:33:36 -0700

    Initial Commit

---
+++ SFTransportation/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  SFTransportation
+//
+//  Created by Kobe Dai on 5/8/14.
+//  Copyright (c) 2014 Jing Dai. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
